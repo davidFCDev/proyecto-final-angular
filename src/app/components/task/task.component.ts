de45c3f12cc2ce2e30fa1d4de20ae656afeecdc1
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-07 16:37:35 +0200

    tasks

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ITask } from 'src/app/models/interfaces/Task.interface';
 
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
-  styleUrls: ['./task.component.scss']
+  styleUrls: ['./task.component.scss'],
 })
-export class TaskComponent {
+export class TaskComponent implements OnInit {
 
+  @Input() task: ITask | undefined;
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
