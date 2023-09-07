cf9ca531c56ff52e7b7e3887886189e0010675e1
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-07 16:15:50 +0200

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskComponent } from './components/task/task.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
+import { LoginFormComponent } from './components/auth/login-form/login-form.component';
+import { RegisterFormComponent } from './components/auth/register-form/register-form.component';
+import { NavBarComponent } from './components/nav/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    TaskComponent,
+    TaskFormComponent,
+    LoginFormComponent,
+    RegisterFormComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
