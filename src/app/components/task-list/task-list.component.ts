e55803a73098877019691fb3d8a2c8387723f0cc
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-07 23:23:17 +0200

    tasklist

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.scss']
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit {
+
+  // TODO Reformular como una lista de tareas
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
