4242338e263b88f80f110889dfb25f8a11aa7e13
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-08 16:54:01 +0200

    styles

---
+++ src/app/modules/lists/basic-list/basic-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-list',
+  templateUrl: './basic-list.component.html',
+  styleUrls: ['./basic-list.component.scss']
+})
+export class BasicListComponent {
+
+}
--- src/app/modules/lists/lists.module.ts
+++ src/app/modules/lists/lists.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BasicListComponent } from './basic-list/basic-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BasicListComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    // Exportamos aquellas clases (componentes/pipes, services, etc) que queremos que sean accesibles desde fuera del módulo
+    BasicListComponent
   ]
 })
 export class ListsModule { }
