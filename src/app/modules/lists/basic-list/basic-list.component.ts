034f006b3d5e211fc8cc8f1b97584eacc3537602
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-11 16:36:32 +0200

    for en objetos

--- src/app/modules/lists/basic-list/basic-list.component.ts
+++ src/app/modules/lists/basic-list/basic-list.component.ts
@@ -1,31 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
+// Tipo base para ejemplo
+export type Product = {
+  name: string;
+  price: number;
+  description: string;
+};
+
 @Component({
   selector: 'app-basic-list',
   templateUrl: './basic-list.component.html',
   styleUrls: ['./basic-list.component.scss'],
 })
 export class BasicListComponent implements OnInit {
-
-  elementsList: string[] = [
-    'Milk',
-    'Bread',
-    'Cheese',
-    'Apples',
-    'Bananas',
-    'Pears',
-    'Butter',
-    'Yogurt',
-    'Cereal',
-    'Eggs',
+  elementsList: Product[] = [
+    {
+      name: 'Milk',
+      price: 1.5,
+      description: 'whole milk',
+    },
+    {
+      name: 'Bread',
+      price: 1.2,
+      description: 'white bread',
+    },
+    {
+      name: 'Eggs',
+      price: 2.5,
+      description: '12 eggs',
+    },
   ];
   loading: boolean = false;
 
   constructor() {}
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   changeLoading() {
     this.loading = !this.loading;
