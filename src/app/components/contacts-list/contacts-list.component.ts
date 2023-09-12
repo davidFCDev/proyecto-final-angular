b4d3e3459933169ab663321e09c3a90e0ae88a1b
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-12 16:36:56 +0200

    render contacts

--- src/app/components/contacts-list/contacts-list.component.ts
+++ src/app/components/contacts-list/contacts-list.component.ts
@@ -5,18 +5,16 @@ import { ContactService } from 'src/app/services/contact.service';
 @Component({
   selector: 'app-contacts-list',
   templateUrl: './contacts-list.component.html',
-  styleUrls: ['./contacts-list.component.scss']
+  styleUrls: ['./contacts-list.component.scss'],
 })
 export class ContactsListComponent implements OnInit {
-
   contactList: IContact[] = [];
 
-  constructor(private contactService: ContactService ) { }
+  constructor(private contactService: ContactService) {}
 
   ngOnInit(): void {
     // Obtener la lista de contactos que brinda el service
 
-    this.contactService.getContacts();
-
+    this.contactList = this.contactService.getContacts();
   }
 }
