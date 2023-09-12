5ab61e93c511d446db2dc12c2eba0b740195a6d5
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-12 15:50:03 +0200

    contactbyid

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@angular/core';
+import { CONTACTS } from '../mocks/contacts.mock';
+import { IContact } from '../models/interfaces/contact.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ContactService {
+  constructor() {}
 
-  constructor() { }
+  getContacts(): IContact[] {
+    return CONTACTS;
+  }
 
-  getContacts() {
-    
+  getContactById(id: number): IContact | void {
+    const contact = CONTACTS.find((contact: IContact) => contact.id === id);
+
+    if (contact) {
+      return contact;
+    } else {
+      console.log('Contact not found');
+    }
   }
 }
