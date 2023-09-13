fd9566fde07fe813218e10e1d41152c428747be9
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-13 16:52:02 +0200

    login

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(email:string, password: string): Observable<any> {
+
+    let body = {
+      email,
+      password
+    }
+
+    // Devolvemos la respuesta del Observable cuando la peticion HTTP termine
+    return this.http.post('http://reqres.in/api/login', body)
+  }
 }
